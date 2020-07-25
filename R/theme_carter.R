3851259ad633a54ef92ad217126a852c2ceae412
carterce1997 <carterce1997@gmail.com>
2020-07-25 12:06:52 -0400

    init

---
+++ R/theme_carter.R
@@ -0,0 +1,14 @@
+
+library(ggplot2)
+
+
+#' A simple ggplot theme.
+theme_carter <- function() {
+
+  theme_minimal() +
+    theme(
+      text = element_text(family = 'Helvetica Neue'),
+      panel.grid = element_blank()
+    )
+
+}
