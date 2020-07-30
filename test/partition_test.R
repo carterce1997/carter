e1dd2b0063f3e1e691f00ef29d4d79d08e5aaf1b
carterce1997 <carterce1997@gmail.com>
2020-07-30 13:45:56 -0400

    modify test

--- test/partition_test.R
+++ test/partition_test.R
@@ -7,8 +7,4 @@ data(iris)
 
 df <-
   iris %>%
-  partition(.proportions = list(train = .99, test = .1), .across_groups = c())
-
-df <-
-  iris %>%
-  partition(.proportions = list(train = .7, validate = .15, test = .15), .across_groups = c(Species))
+  partition(.proportions = list(train = .7, validate = .15, test = .16), .across_groups = c(Species))
