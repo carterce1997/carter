ffa2126c6f55a7765de9390971efcdcab0a8907a
Chris Carter <carterce1997@gmail.com>
2021-06-16 10:52:19 -0400

    theme

--- R/theme_carter.R
+++ R/theme_carter.R
@@ -8,6 +8,19 @@ theme_carter <- function() {
     theme(
       text = element_text(family = 'sans'),
       panel.grid.minor = element_blank(),
+      axis.line = element_line(colour = 'gray'),
+    )
+
+}
+
+#' A simple ggplot theme.
+theme_carter2 <- function() {
+
+  theme_minimal() +
+    theme(
+      text = element_text(family = 'sans'),
+      panel.grid = element_blank(),
+      axis.line = element_line(colour = 'gray'),
     )
 
 }
--- test/theme_carter.R
+++ test/theme_carter.R
@@ -7,4 +7,5 @@ iris %>%
   janitor::clean_names() %>%
   ggplot() +
   geom_point(aes(x = sepal_length, y = sepal_width, color = species)) +
-  theme_carter()
+  ggtitle('Iris') +
+  theme_carter2()
